72c2cb8b939d509efae188667189f22bb20405fe
sonumeewa <sonumeewa@gmail.com>
2020-03-21 22:56:54 +0530

    react

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import App from './App';
+
+class Header extends Component {
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'Hello there, this is a React component'
+    );
+  }
+}
+export default App;
