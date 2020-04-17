762d5ecdf7af383f8d7cb24a42f1b951930e4746
Vipul Barodiya <sonumeewa@gmail.com>
2020-04-17 17:37:09 +0530

    merge

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
@@ -60,66 +59,3 @@ ProfileTop.propTypes = {
 };
 
 export default ProfileTop;
-=======
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
-
-const ProfileTop = ({
-  profile: {
-    status,
-    company,
-    occasion,
-    website,
-    location,
-    social,
-    user: { name, avatar },
-  },
-}) => {
-  return (
-    <div className='profile-top bg-primary p-2'>
-      <img className='round-img my-1' src={avatar} alt='' />
-      <h1 className='large'>{name}</h1>
-      <p className='lead'>
-        {status} {company && <span>at {company}</span>}
-      </p>
-      <p>{location && <span>at {location}</span>}</p>
-
-      <div className='icons my-1'>
-        {website && (
-          <a href={website} target='_blank' rel='noopener noreferrer'>
-            <i className='fas fa-globe fa-2x'></i>
-          </a>
-        )}
-        {social && social.facebook && (
-          <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-facebook fa-2x'></i>
-          </a>
-        )}
-        {social && social.youtube && (
-          <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-youtube fa-2x'></i>
-          </a>
-        )}
-        {social && social.instagram && (
-          <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-instagram fa-2x'></i>
-          </a>
-        )}
-        {social && social.instagram && (
-          <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-instagram fa-2x'></i>
-          </a>
-        )}
-      </div>
-    </div>
-  );
-};
-
-ProfileTop.propTypes = {
-  profile: PropTypes.object.isRequired,
-};
-
-export default ProfileTop;
->>>>>>> a45396b7fd8624fbb25d4585ee30a3f28d79bc47
