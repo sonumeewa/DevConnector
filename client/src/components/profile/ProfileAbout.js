3794409b2857a02e3e4e8fc4a53934a7bd231bc5
Vipul Barodiya <sonumeewa@gmail.com>
2020-04-17 17:36:47 +0530

    merge

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
@@ -35,41 +34,3 @@ ProfileAbout.propTypes = {
 };
 
 export default ProfileAbout;
-=======
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-
-const ProfileAbout = ({
-  profile: {
-    bio,
-    skills,
-    user: { name },
-  },
-}) => {
-  return (
-    <div class='profile-about bg-light p-2'>
-      {bio && (
-        <Fragment>
-          {' '}
-          <h2 class='text-primary'>{name.trim().split(' ')[0]}'s Bio</h2>
-          <p>{bio}</p>
-        </Fragment>
-      )}
-      <h2 className='text-primary'>Skill Set</h2>
-      <div className='skills'>
-        {skills.map((skill, index) => (
-          <div key={index} className='p-1'>
-            <i className='fas fa-check primary'> {skill}</i>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-ProfileAbout.propTypes = {
-  profile: PropTypes.object.isRequired,
-};
-
-export default ProfileAbout;
->>>>>>> a45396b7fd8624fbb25d4585ee30a3f28d79bc47
